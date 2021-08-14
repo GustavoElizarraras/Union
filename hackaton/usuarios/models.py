b9d3fa95315c9e7f97ceba760ff2c554fee580fa
Brandon Diaz <branal845diaz@gmail.com>
2021-08-14 18:24:42 -0500

    Se crearon los modelos de las aplicacion optimizacion y alumno

--- hackaton/usuarios/models.py
+++ hackaton/usuarios/models.py
@@ -1,5 +1,8 @@
+from django.db.models.deletion import CASCADE
+from hackaton.optimizacion.models import Escuela
 from django.contrib.auth.models import User
 from django.db import models
+from optimizacion.models import Materia, Licenciatura
 
 class Alumno(models.Model):
     """Alumno model.
@@ -9,6 +12,9 @@ class Alumno(models.Model):
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    materias = models.ForeignKey(Materia, on_delete=CASCADE)
+    escuela = models.ForeignKey(Escuela, on_delete=CASCADE)
+    licenciatura = models.ForeignKey(Licenciatura, on_delete=CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
