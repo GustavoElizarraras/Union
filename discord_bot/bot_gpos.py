4b61d8ba6774d09f1f0762810b454caa5c0e6728
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-08-15 18:21:48 +0000

    Cleaned script, still functional

--- discord_bot/bot_gpos.py
+++ discord_bot/bot_gpos.py
@@ -7,8 +7,6 @@ load_dotenv()
 token = os.getenv('token')
 bot = commands.Bot(command_prefix='!')
 
-
-target_channel_id = 876304013975519255
 channel_name = "planificación-de-sesiones"
 
 @bot.event
@@ -35,8 +33,6 @@ async def called_every_min():
 async def before():
     print("waiting")
     await bot.wait_until_ready()
-    print(bot.get_channel(target_channel_id))
-
 
 @bot.event
 async def on_message(message):
@@ -45,19 +41,6 @@ async def on_message(message):
 
     if message.content.startswith("$mello"):
         await message.channel.send("Hello world, first discord bot")
-
-
-
-# @bot.command(pass_context=True)
-# async def broadcast(ctx, *, msg):
-#     for server in bot.servers:
-#         for channel in server.channels:
-#             try:
-#                 await bot.send_message(channel, msg)
-#             except Exception:
-#                 continue
-#             else:
-#                 break
 
 bot.loop.create_task(before())            
 called_every_min.start()            
