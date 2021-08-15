df905a078e56382d3beffa5faa20dd24eb427b77
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-08-15 16:21:55 +0000

    modified for the bot class

--- discord_bot/bot_gpos.py
+++ discord_bot/bot_gpos.py
@@ -5,7 +5,6 @@ import os
 
 load_dotenv()
 token = os.getenv('token')
-client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 
@@ -18,7 +17,7 @@ async def on_ready():
 
 @tasks.loop(seconds=10.0)
 async def called_every_min():
-    message_channel = client.get_channel(target_channel_id)
+    message_channel = bot.get_channel(target_channel_id)
     print(message_channel)
     print(f"Got channel {message_channel}")
     await message_channel.send("Your message")
@@ -26,11 +25,11 @@ async def called_every_min():
 @called_every_min.before_loop
 async def before():
     print("waiting")
-    await client.wait_until_ready()
-    print(client.get_channel(target_channel_id))
+    await bot.wait_until_ready()
+    print(bot.get_channel(target_channel_id))
 
 
-@client.event
+@bot.event
 async def on_message(message):
     if message.author == client.user:
         return
@@ -50,7 +49,8 @@ async def on_message(message):
 #                 continue
 #             else:
 #                 break
-client.loop.create_task(before())            
+
+bot.loop.create_task(before())            
 called_every_min.start()            
-client.run(token)
+bot.run(token)
 
