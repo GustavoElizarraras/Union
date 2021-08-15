3e9433d4f47830bae00609dd4a5032884a813e2a
GustavoElizarraras <angelizarrarasg@gmail.com>
2021-08-15 16:16:42 +0000

    every 10s the bot send a message to a channel

--- discord_bot/bot_gpos.py
+++ discord_bot/bot_gpos.py
@@ -1,21 +1,33 @@
 import discord
 from discord.ext import commands, tasks
+from dotenv import load_dotenv
+import os 
 
+load_dotenv()
+token = os.getenv('token')
 client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
-target_channel_id = 876304013975519259
 
-@tasks.loop(minutes=1)
+target_channel_id = 876304013975519255
+
+@bot.event
+async def on_ready():
+    print(bot.user.name)
+    print(bot.user.id)
+
+@tasks.loop(seconds=10.0)
 async def called_every_min():
-    message_channel = bot.get_channel(target_channel_id)
+    message_channel = client.get_channel(target_channel_id)
+    print(message_channel)
     print(f"Got channel {message_channel}")
     await message_channel.send("Your message")
 
 @called_every_min.before_loop
 async def before():
-    await bot.wait_until_ready()
-    print("Finished waiting")
+    print("waiting")
+    await client.wait_until_ready()
+    print(client.get_channel(target_channel_id))
 
 
 @client.event
@@ -23,20 +35,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
+    if message.content.startswith("$mello"):
         await message.channel.send("Hello world, first discord bot")
 
-@bot.command(pass_context=True)
-async def broadcast(ctx, *, msg):
-    for server in bot.servers:
-        for channel in server.channels:
-            try:
-                await bot.send_message(channel, msg)
-            except Exception:
-                continue
-            else:
-                break
-            
+
+
+# @bot.command(pass_context=True)
+# async def broadcast(ctx, *, msg):
+#     for server in bot.servers:
+#         for channel in server.channels:
+#             try:
+#                 await bot.send_message(channel, msg)
+#             except Exception:
+#                 continue
+#             else:
+#                 break
+client.loop.create_task(before())            
 called_every_min.start()            
-client.run("ODc2Mjk4OTQ3NDE1NjYyNjUz.YRiC_Q.qED2aVic72pg1UKiQQg6mgOakVM")
+client.run(token)
 
